58505be0f7dd3b6efdae74eeea81e63e3106b18f
lyschoening <lars@lyschoening.de>
2014-01-27 13:10:44 +0100

    version bumped to 1.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Lars Schöning',
     author_email='lays@biosustain.dtu.dk',
     name='Flask-Presst',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['*tests*']),
     url='https://github.com/biosustain/flask-presst',
     license='MIT',
