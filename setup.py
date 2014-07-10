0d6b040ce7724a27d8aedc0a13c1c901eef0cc75
lyschoening <lars@lyschoening.de>
2014-07-10 15:39:46 +0200

    Add Flask-Testing to setup requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     test_suite='nose.collector',
     description='REST API framework for Flask and SQLAlchemy',
     setup_requires=[
+        'Flask-Testing',
         'nose>=1.1.2',
     ],
     install_requires=[
